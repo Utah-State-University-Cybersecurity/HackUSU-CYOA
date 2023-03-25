eca7cd99924c4be71f6c151b2e1d8d6564bf0e8c
Chandler M <115592179+spacemagicmango@users.noreply.github.com>
2023-03-25 08:57:24 -0600

    Add files via upload

---
+++ hackUSU proj/looptest.py
@@ -0,0 +1,9 @@
+
+
+input('Welcome to your SuperStory! \n\n-Press any key to continue-')
+
+# possible terminal output clearing command here
+
+print('First, choose your character!')
+# output character list from AI
+input('-Choose a number-')
